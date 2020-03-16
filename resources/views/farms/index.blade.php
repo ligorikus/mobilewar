0bfdc3d747c709fa77e14c718f04b863a12d45e6
ligorikus <semal551@gmail.com>
2020-03-16 17:29:55 +0600

    farms view

--- resources/views/farms/index.blade.php
+++ resources/views/farms/index.blade.php
@@ -4,7 +4,8 @@
     @foreach($map_field->farms as $farm)
         <div>
             <a href="{{route('farms.view', $farm)}}">
-                {{trans('farms.'.$farm->farm_level->farm->title)}}
+                {{trans('farms.'.$farm->farm_level->farm->title)}}.
+                {{trans('builds.level')}} {{$farm->farm_level->level}}
             </a>
 
         </div>
